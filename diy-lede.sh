b41bca81982d04aec3b3c80423f3bb7bf3e9a53e
Shiori <106437447+RustyCore856@users.noreply.github.com>
2023-01-18 13:23:43 +0800

    Update diy-lede.sh

--- diy-lede.sh
+++ diy-lede.sh
@@ -7,6 +7,9 @@
 # Blog: https://p3terx.com
 #===============================================
 
+# Clear the login password
+sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-settings/files/zzz-default-settings
+
 # 修改默认IP
 sed -i 's/192.168.1.1/10.0.0.2/g' package/base-files/files/bin/config_generate
 
